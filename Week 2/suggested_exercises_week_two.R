9159e89cb234b0274417a569fc8f3e6e4b2876e7
Alexander Aragão <aragao.alexander@gmail.com>
2021-07-23 18:01:25 -0300

    Finished Week 1 and started working on Week 2 exercises

---
+++ Week 2/suggested_exercises_week_two.R
@@ -0,0 +1,14 @@
+## Script for the suggested exercises in Week 1 of Duke University's 
+## Inferential Statistics course on Coursera.
+## Exercises are taken from Open Intro Statistics, 3rd ed.
+## Author: Alexander Aragão
+
+##Suggested reading: Chapter 4, Section 4.3, 4.4, 4.5, 4.6 (excluding Section 4.6.2)
+
+##Suggested exercises: (End of chapter exercises from OpenIntro Statistics)
+
+##Hypothesis tests: 4.17, 4.19, 4.23, 4.25, 4.27
+##Inference for other estimators: 4.43, 4.45
+##Decision errors, significance, and confidence: 4.29, 4.31, 4.47
+
+##  Variability in Estimates --------------------------------------------------
