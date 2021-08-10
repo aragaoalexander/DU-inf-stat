c933fc963450b80d2167fb53efc98b03ddff3844
Alexander Aragão <aragao.alexander@gmail.com>
2021-08-10 16:17:40 -0300

    FInished Week 4 suggested exercises

--- Week 4/suggested_exercises_week_four.R
+++ Week 4/suggested_exercises_week_four.R
@@ -161,61 +161,249 @@ n_calc
 
 ## 6.7.2 Difference of two proportions ---------------------------------------
 
+# 6.23 Social experiment, Part I.
+# There's not enough successes under the provocative scenario, which means that
+# the sample size isn't large enough to be significant.
 
-# 5.45 Coffee, depression and physical activity
-# a) H0: All means are equal. HA: At least one pair of means differs from each other.
-# b) We can assume random sampling was made, meaning that the observations are independent from each other. Normality may
-# not be significant, since the values are never negative, so a right skew can be expected. The variances are almost equal
-# between groups.
+# 6.25 Gender and color preference
+# a) False.
+# b) True.
+# c) True.
+# d) True.
+# e) False.
+
+# 6.27 Public option, Part III.
+# a) CI is (23.3%, 32.7%)
+p1 = 0.7
+n1 = 819
+p2 = 0.42
+n2 = 783
+se = sqrt(p1*(1-p1)/n1 + p2*(1-p2)/n2)
+se
+z = qnorm(0.025, lower.tail = FALSE)
+lower = (p1-p2) - z*se
+upper = (p1-p2) + z*se
+lower
+p1-p2
+upper
+
+# b) True.
+
+# 6.29 Offshore drilling, Part I
+# a) 
+p1 = 104/438
+p1
+n1 = 438
+p2 = 131/389
+p2
+n2 = 389
+
+# b) pvalue is low (5e-6), which means that we can reject H0 and thus conclude
+# that there is a difference between the proportions of "Don't know" for graduates.
+p_pool = (p1*n1+p2*n2)/(n1+n2)
+p_pool
+
+se = sqrt(p_pool*(1-p_pool)/n1 + p_pool*(1-p_pool)/n2)
+se
+
+z = (p1-p2)/se
+z
+
+pvalue = pnorm(z)
+pvalue
+
+# 6.31 Offshore drilling, Part II
+# a)
+p1 = 154/438
+n1 = 438
+p2 = 132/389
+n2 = 389
+p1
+p2
+
+# b) pvalue is high (0.64), which means that we can't reject H0 and thus conclude
+# that there is no difference between the proportions of "Support" for graduates.
+p_pool = (p1*n1+p2*n2)/(n1+n2)
+p_pool
+
+se = sqrt(p_pool*(1-p_pool)/n1 + p_pool*(1-p_pool)/n2)
+se
+
+z = (p1-p2)/se
+z
+
+pvalue = pnorm(z)
+pvalue
+
+# 6.33 Sleep deprived transportation workers
+# p-value is relatively low (0.049), so we could reject H0 and conclude that there's
+# a difference between sleep deprived truck drivers and the control group for a
+# SL = 5%. However, we might be making a Type I error since p-value almost equal to the SL.
+
+p1 = 35/292
+n1 = 292
+p2 = 35/203
+n2 = 203
+p_pool = (p1*n1+p2*n2)/(n1+n2)
+p_pool
+
+se = sqrt(p_pool*(1-p_pool)/n1 + p_pool*(1-p_pool)/n2)
+
+z = (p1-p2)/se
+
+pvalue = pnorm(z)
+pvalue
+
+# 6.35 HIV in sub-Saharan Africa.
+# a)
+n1 = 120
+n2 = 120
+n = n1+n2
+p1 = 26/120
+p2 = 10/120
+
+p_pool = (p1*n1+p2*n2)/(n1+n2)
+
+hiv <- tibble(
+  result = c("virologic failure", "no symptoms"),
+  nevaripine = c(p1*n1, (1-p1)*n1),
+  lopinavir = c(p2*n2, (1-p2)*n2),
+  )
+
+# b) H0: p1 = p2, HA: p1 != p2
 # c)
+# Conditions: 1. Independence: random sampling check, n < 10% of population check.
+#             2. Sample size: successes and failures over 10 observations in each treatment.
 
-cf_n = 50739
-cf_k = 5
-cf_m1 = 18.7
-cf_m2 = 19.6
-cf_m3 = 19.3
-cf_m4 = 18.9
-cf_m5 = 17.5
-cf_sd1 = 21.1
-cf_sd2 = 25.5
-cf_sd3 = 22.5
-cf_sd4 = 22
-cf_sd5 = 22
-cf_n1 = 12215
-cf_n2 = 6617
-cf_n3 = 17234
-cf_n4 = 12290
-cf_n5 = 2383
+se = sqrt(p_pool*(1-p_pool)/n1 + p_pool*(1-p_pool)/n2)
 
-cf_mean <- c(cf_m1, cf_m2, cf_m3, cf_m4, cf_m5)
-cf_sd <- c(cf_sd1, cf_sd2, cf_sd3, cf_sd4, cf_sd5)
-cf_n <- c(cf_n1, cf_n2, cf_n3, cf_n4, cf_n5)
+z = (p1-p2)/se
+z
+pvalue = pnorm(z, lower.tail = FALSE)*2
+pvalue
 
-coffee <- data.frame(cf_mean, cf_sd, cf_n,
-                     row.names = c("one_cup_wk", "two_six_cup_wk", "one_cup_day", "two_three_cup_day", "four_cup_day")
+#pvalue is 0.004, which very LOW. This means that we CAN reject H0, that is,
+# nevaripine and lopinavir have different proportions of virologic failure in
+# treated women.
+
+## 6.7.3 Testing for goodness of fit using chi-square --------------------------
+
+# 6.39 True or false, part I.
+# a) False, chi-square has one parameter: degrees of freedom.
+# b) True.
+# c) True.
+# d) False, it becomes less skewed (more symmetrical).
+
+# 6.41 
+# a) H0: there was no difference between the professor's predictions and what the
+# students used in class. HA: There was a difference.
+# b)
+textbook <- tibble(
+  format = c("hard copy", "print", "online"),
+  observed = c(71, 30, 25),
+  expected = c(0.6*126, 0.25*126, 0.15*126)
 )
 
-cf_dft = sum(coffee$cf_n)-1
-cf_dfg = nrow(coffee)-1
-cf_dfe = cf_dft-cf_dfg
-cf_xbar = sum(coffee$cf_mean*coffee$cf_n)/sum(coffee$cf_n)
-cf_sse = 25564819
-cf_sst = 25575327
-cf_ssg = sum(coffee$cf_n*(coffee$cf_mean-cf_xbar)^2)
+# c) Conditions:
+# 1. Independence: random assignment (not check, since they are all from the same class)
+# n < 10% of all students, each result is one cell in the table.
+# 2. Sample size check (more than 5 expected cases for each scenario)
 
-cf_msg = cf_ssg/cf_dfg
-cf_mse = cf_sse/cf_dfe
+# d)
+chi = sum((textbook$observed-textbook$expected)^2/textbook$expected)
+df = (nrow(textbook)-1)*(ncol(textbook)-2)
 
-cf_F = cf_msg/cf_mse
-cf_pvalue = pf(cf_F, cf_dfg, cf_dfe, lower.tail = FALSE)
+pchisq(q = chi, df = df, lower.tail = FALSE)
 
-coffee_anova = data.frame(Df = c(cf_dfg, cf_dfe, cf_dft),
-                          Sum_Sq = c(cf_ssg, cf_sse, cf_sst),
-                          Mean_Sq = c(cf_msg, cf_mse, 0),
-                          F_value = c(cf_F, 0, 0),
-                          Pr = c(cf_pvalue, 0, 0),
-                          row.names = c("coffee","residuals", "total")
+# e) p-value was 0.31, which menas that we can't reject H0. In other words, there's
+# no difference between the professor's expectations and the observations.
+
+# 6.43 Rock-paper-scissors
+total = 43+21+35
+rps <- data.frame(
+  observed = c(43, 21, 35),
+  expected = c(total/3, total/3, total/3),
+  row.names = c("rock", "paper", "scissors")
+)
+chi = sum((rps$observed-rps$expected)^2/rps$expected)
+chi
+df = (nrow(rps)-1)*(ncol(rps)-1)
+df
+pvalue = pchisq(q = chi, df = df, lower.tail = FALSE)
+pvalue
+# P-value was 0.02, which is lower than 0.05. At a 95% confidence level, we can
+# say that the observations were different from expectations.
+
+## 6.7.4 Testing for independence in two-way tables ----------------------------
+# 6.45 Quitters
+# a)
+quitters <- data.frame(
+  patch_support = c(40, 150-40),
+  no_support = c(30, 150-30),
+  row.names = c("quit smoking", "didn't quit smoking")
 )
 
-# d) H0 is rejected, i.e., it means that there's a difference between at least one of the groups.
+quitters <- quitters %>%
+  mutate (total = patch_support + no_support)
 
+# b)
+
+quitters <- quitters %>%
+  mutate (patch_support_exp = (sum(patch_support)*total)/sum(total),
+          no_support_exp = (sum(no_support)*total)/sum(total))
+
+quitters <- quitters %>%
+    mutate(obs_higher_than_exp_PS = ifelse(patch_support>patch_support_exp, "yes", "no"),
+           obs_higher_than_exp_NS = ifelse(no_support>no_support_exp, "yes", "no")
+           )
+
+# 6.47 Offshore drilling, Part III
+
+drill <- data.frame(
+  college_yes = c(154, 180, 104),
+  college_no = c(132, 126, 131),
+  row.names = c("support", "oppose", "do not know")
+)
+
+chisq <- chisq.test(drill)
+chisq$observed
+round(chisq$expected,2)
+chisq$p.value
+
+#p-value is 0.003, which means that we can reject H0, in other words, there is a
+# difference in responses from graduates and non-graduates.
+
+## 6.7.5 Small sample hypothesis testing for a proportion ----------------------
+
+# 6.51 Bullying in schools.
+# No, it's not. There's only 9 successes in the sample, meaning that the Sample
+# Size condition isn't fullfilled.
+
+# 6.53 The Egyptian Revolution.
+# a) H0: p_hs = 0.69, HA: p_hs != 0.69
+# b)
+p_hs = 17/30
+p_hs
+
+# c) Supposing the null hypothesis is true, we'd have 21 successes and 9 failures,
+# which doesn't fulfill the sample size condition.
+succ = 0.69*30
+fail = (1-0.69)*30
+
+# d)
+pvalue = pbinom(17, 30, 0.69)*2
+pvalue
+
+# pvalue is 0.10, which is higher than 0.05 (CL 95%). This means that we can't reject
+# H0, or that we can't reject the hypothesis that the proportion of HS students
+# who followed news about Egypt is 69%.
+
+# e) ????
+
+#6.55 Social Experiment, Part II
+# a) H0: ppr - pcon = 0, HA: ppr - pcon != 0.
+# b)
+ppr = 5/20
+pcon = 15/25
+ppr - pcon
+# c) estimated pvalue = 0.04
